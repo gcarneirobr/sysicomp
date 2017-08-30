34a53997fe8edf6e345bfa292a84f1b9826d88d0
Pedro Frota <pvmf@icomp.ufam.edu.br>
2017-08-30 17:25:10 -0400

    Adição do ambiente "attendance"

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -2,4 +2,5 @@
 Yii::setAlias('common', dirname(__DIR__));
 Yii::setAlias('frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('backend', dirname(dirname(__DIR__)) . '/backend');
+Yii::setAlias('attendance', dirname(dirname(__DIR__)) . '/attendance');
 Yii::setAlias('console', dirname(dirname(__DIR__)) . '/console');
